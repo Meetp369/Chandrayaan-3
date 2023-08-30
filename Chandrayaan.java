bc8229f44e4e5290e0c87f20293495362cd31326
Meet <meetp0878@gmail.com>
2023-08-30 10:32:44 +0530

    unit test - starting position and direction checked

--- Chandrayaan.java
+++ Chandrayaan.java
@@ -1,9 +1,28 @@
 /**
  * Chandrayaan
  */
+class GetDirectionAndPosition{
+    int x,y,z;
+    String direction;
+
+    
+    public GetDirectionAndPosition(int x,int y,int z,String direction){
+
+        this.x=x;
+        this.y=y;
+        this.z=z;
+        this.direction=direction;
+
+    }
+
+}
 public class Chandrayaan {
 
     public static void main(String[] args) {
+
+        int[] startingPosition = {0, 0, 0};
+        String startingDirection ="N";
         
+        GetDirectionAndPosition gdp= new GetDirectionAndPosition(startingPosition[0], startingPosition[1],startingPosition[2], startingDirection);
     }
 }
--- ChandrayaanTest.java
+++ ChandrayaanTest.java
@@ -1,8 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class ChandrayaanTest {
-    @Test
-    public void testMain() {
 
+    @Test
+    public void startingDirectionAndPosition() {
+        GetDirectionAndPosition gdp= new GetDirectionAndPosition(0, 0, 0, "N");
+        assertTrue((gdp.direction.equals("N" )) && (gdp.x==0)&& (gdp.y==0)&& (gdp.z==0) );
     }
 }
